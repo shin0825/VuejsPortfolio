b0b176b329632f8d02b97825fc9630afe8085729
shin0825 <43142294+shin0825@users.noreply.github.com>
2019-07-17 23:35:51 +0900

    エラーが出るので自動カウントは削除

--- src/components/NowDate.vue
+++ src/components/NowDate.vue
@@ -8,23 +8,8 @@
 export default {
   data: function () {
     return {
-      nowDate: new Date()
+      showDate: new Date().toLocaleDateString().replace(/\//g, '.')
     }
   },
-  computed: {
-    showDate: function () {
-      return this.nowDate.toLocaleString().replace(/\//g, '.')
-    }
-  },
-  methods: {
-    refresh: function () {
-      this.nowDate = new Date()
-      const self = this
-      setTimeout(() => { self.refresh() }, 1000)
-    }
-  },
-  created: function () {
-    this.refresh()
-  }
 }
 </script>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -36,7 +36,6 @@
         <div class="p-3 bg-white">
           <p class="card-context">
             <span class="text-primary">2019.06</span>
-              - <span>Web家計簿</span>
             <br>
             <span class="badge badge-primary">HTML</span>
             <span class="badge badge-primary">CSS</span>
@@ -44,11 +43,15 @@
             <span class="badge badge-primary">Bootstrap4</span>
             <span class="badge badge-primary">RubyonRails</span>
             <span class="badge badge-primary">PostgreSQL</span>
-            <img class="img-fluid img-thumbnail mx-auto mt-2 mb-2" src="../assets/kakeibo.png" alt="">
             <br>
-            簡単な家計簿です。お財布ごとに出費・収入ログを集計しているので、財布の状況をみることもできます。
           </p>
-
+          <figure>
+            <img class="img-fluid img-thumbnail mx-auto mt-2 mb-2" src="../assets/kakeibo.png" alt="">
+            <figcaption>
+              <h3>Web家計簿</h3>
+              <p>簡単な家計簿です。お財布ごとに出費・収入ログを集計しているので、財布の状況をみることもできます。</p>
+            </figcaption>
+          </figure>
           <p class="card-context mt-1">
             <span class="text-primary">2019.04 - 2019.05</span>
               - <span>Railsチュートリアル</span>
@@ -78,23 +81,51 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: ['nowDate'],
-  computed: {
-    showDate: function () {
-      return this.nowDate.toLocaleString()
-    }
-  },
-  methods: {
-    refresh: function () {
-      this.nowDate = new Date()
-      const self = this
-      setTimeout(() => { self.refresh() }, 1000)
-    }
-  },
-  created: function () {
-    this.refresh()
-  }
+<style>
+figure {
+  position: relative;
+  overflow: hidden;
+  width: 400px;
 }
-</script>
+figcaption {
+  text-align: right;
+  vertical-align: bottom;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,.6);
+  transition: .3s;
+  opacity: 0;
+}
+figcaption h3,
+figcaption p {
+  color: blue;
+  position: absolute;
+  left: -100%;
+  width: 360px;
+  transition: .3s;
+}
+figcaption h3 {
+  top: 30px;
+}
+figcaption p {
+  top: 75px;
+}
+figure:hover figcaption {
+  opacity: 1;
+}
+figure:hover figcaption h3,
+figure:hover figcaption p {
+  color: white;
+  left: 20px;
+}
+figure:hover figcaption h3 {
+  transition-delay: .2s;
+}
+figure:hover figcaption p {
+  transition-delay: .5s;
+}
+</style>
