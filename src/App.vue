bcd1a225b9534ebf106e15ed6376fc95acd610d0
s.nakamura <43142294+shin0825@users.noreply.github.com>
2020-06-02 02:02:43 +0900

    Update App.vue
    add my rss link

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/skills">Skills</router-link> |
       <router-link to="/kakeibo">Kakeibo</router-link> |
-      <a href="https://otogemap.herokuapp.com" target="_blank">IIDX DB</a>
+      <a href="https://otogemap.herokuapp.com" target="_blank">IIDX DB</a> |
+      <a href="https://nameless-tundra-72223.herokuapp.com/" target="_blank">My RSS</a>
     </div>
     <div id="main" >
       <transition name="router-transition">
